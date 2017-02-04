a9d7e3c0dcda8c04d1cc24a88a869ee0bab31668
Isy <isaacmansfield@gmail.com>
2017-02-04 14:37:38 -0800

    make the app port-aware for heroku

--- app.py
+++ app.py
@@ -10,4 +10,6 @@ def pork():
 	return "Human hair is underrepresented in government."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.debug = True
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
