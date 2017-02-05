27725e6bbe71a3c99be446ba077fa7425bfb347d
Isy <isaacmansfield@gmail.com>
2017-02-04 17:39:26 -0800

    Add SMS route

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask
+from flask import request
+import logging
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World yo yo!"
+    return 'Hello World yo yo!'
 
-@app.route("/pork")
+@app.route('/pork')
 def pork():
-	return "Human hair is underrepresented in government."
+	return 'Human hair is underrepresented in government.'
 
-if __name__ == "__main__":
+@app.route('/sms', methods=['POST'])
+def sms():
+	app.logger.info(request.form)
+	return 'blah'
+
+if __name__ == '__main__':
 	app.debug = True
-	port = int(os.environ.get("PORT", 5000))
+	port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port)
