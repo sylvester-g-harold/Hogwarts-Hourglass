de7222c662dbc904bcadf94c39fc5f41ad465733
Isy <isaacmansfield@gmail.com>
2017-02-05 13:39:43 -0800

    Add database support

--- app.py
+++ app.py
@@ -4,11 +4,24 @@ from flask import request
 import logging
 import os
 import sms_parser
+import psycopg2
 
 app = Flask(__name__)
-cache = {
-	'messages': []
-}
+
+def connect_db():
+	db_conn = psycopg2.connect(
+		database=os.environ.get('db_name', 'hoggyhoggywartswarts'),
+		user='isy',
+		password='')
+	return db_conn
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'postgres_db'):
+        g.postgres_db = connect_db()
+    return g.postgres_db
 
 @app.route('/')
 def hello():
@@ -17,6 +30,53 @@ def hello():
 @app.route('/pork')
 def pork():
 	return 'Human hair is underrepresented in government.'
+
+def is_valid_point_value_change(point_value_change):
+	return point_value_change['house'] and point_value_change['points']
+
+def store_point_value_change(message, professor_name, point_value_change):
+	"""
+	Sample working SQL insert statement
+	insert into hourglass_points 
+		(house, points, professor_name, message)
+		values ('gryffindor', -3, 'floyd', '3 points from gryffindor! good job bu!');
+
+	Sample `point_value_change` value:
+	{
+	    'house': 'gryffindor',
+	    'points': 5
+    }	
+	"""
+	house = point_value_change['house']
+	points = point_value_change['points']
+
+	db = get_db()
+	cursor = db.cursor()
+
+	statement = ('insert into hourglass_points '
+				 '(house, points, professor_name, message)'
+				 'values (\'{}\', {}, \'{}\', \'{}\');'
+				).format(house, points, professor_name, message)
+
+	cursor.execute(statement)
+	db.commit()
+
+	app.logger.info('Inserted values into db: {}, {}, {}, {}'
+		.format(house, points, professor_name, message))
+
+def get_professor_name_from_phone_number(phone_number):
+	# TODO: actually look up the name from this phone number
+	return phone_number
+
+@app.route('/dump_db', methods=['GET'])
+def dump():
+	db = get_db()
+	cursor = db.cursor()
+	cursor.execute("SELECT * FROM hourglass_points;")
+	result_set = cursor.fetchall()
+	msg = '--- FETCH FROM DB ---\n' + str(result_set)
+	app.logger.info(msg)
+	return str(result_set)
 
 @app.route('/sms', methods=['POST'])
 def sms(): 
@@ -35,25 +95,30 @@ def sms():
 	 ('To', u'+15107571733'),
 	 ('AccountSid', u'ACfc502b87d78195dde44c7d952f835c7e'),
 	 ('ToZip', u'94571'),
-	 ('Body', u'hey man'),
+	 ('Body', u'5 points to gryffindor'),
 	 ('SmsSid', u'SM7d046bca6423d27801e50f1a323152a6'),
 	 ('MessageSid', u'SM7d046bca6423d27801e50f1a323152a6'),
 	 ('FromState', u'CA'),
 	 ('ApiVersion', u'2010-04-01'),
 	 ('FromCountry', u'US')]
 	"""
+	# Print the payload from the client.
 	app.logger.info(request.form)
-
+	professor_name = get_professor_name_from_phone_number(request.form['From'])
 	body = request.form['Body']
+
+
 	point_value_change = sms_parser.parse_point_value_change(body)
-	if point_value_change['house'] and point_value_change['points']:
-		app.logger.info('Parsed: {}'.format(point_value_change))
-		cache['messages'].append(point_value_change)
-		app.logger.info('Cache: {}'.format(cache))
+
+
+	if is_valid_point_value_change(point_value_change):
+		app.logger.info('Storing: {}'.format(point_value_change))
+		store_point_value_change(body, professor_name, point_value_change)
 	else:
 		app.logger.warning('Unable to parse {}'.format(body))
 
 	return 'ok'
+
 
 if __name__ == '__main__':
 	app.debug = True
